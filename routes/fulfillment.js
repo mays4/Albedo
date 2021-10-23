0c8f05c440b27c4d2559e238efa8aa89a4b4f4bf
Hasan Lam <hcb.lam@gmail.com>
2021-10-23 20:16:17 +0000

    Set up basic initial fulfillment route

---
+++ routes/fulfillment.js
@@ -0,0 +1,11 @@
+/*
+ * All routes for fulfillment are defined here
+ * Since this file is loaded in server.js into api/fulfillment,
+ *   these routes are mounted onto /fulfillment
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+
+
+
+
--- server.js
+++ server.js
@@ -37,11 +37,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+// const fulfillRoutes = require("./routes/fulfillment");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+// app.use("/api/fulfillment", fulfillRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -51,6 +53,11 @@ app.use("/api/widgets", widgetsRoutes(db));
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+// Temporary fulfillment route
+app.get("/fulfillment", (req, res) => {
+  res.render('fulfillment');
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
